9f2910da189508479bd47f2e68d7f9f8f12da0fc
Luiz Chagas <luizchagasjr@gmail.com>
2021-11-01 14:57:20 -0500

    v1.3.0

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 const axios = require('axios');
 
-const getSingleStockInfo = stock =>
-  new Promise((resolve, reject) => {
-    if (!stock) {
-      return reject(Error('Stock symbol required'));
-    }
-    if (typeof stock !== 'string') {
-      return reject(Error(`Invalid argument type. Required: string. Found: ${typeof stock}`));
-    }
+const getSingleStockInfo = (stock) => new Promise((resolve, reject) => {
+  if (!stock) {
+    return reject(Error('Stock symbol required'));
+  }
+  if (typeof stock !== 'string') {
+    return reject(
+      Error(`Invalid argument type. Required: string. Found: ${typeof stock}`),
+    );
+  }
 
-    const url = `https://query1.finance.yahoo.com/v7/finance/quote?symbols=${stock}`;
+  const url = `https://query1.finance.yahoo.com/v7/finance/quote?symbols=${stock}`;
 
-    return axios
-      .get(url)
-      .then((res) => {
-        const { data } = res;
-        if (
-          !data ||
-          !data.quoteResponse ||
-          !data.quoteResponse.result ||
-          data.quoteResponse.result.length === 0
-        ) {
-          return reject(new Error(`Error retrieving info for symbol ${stock}`));
-        }
-        return resolve(data.quoteResponse.result[0]);
-      })
-      .catch(err => reject(err));
-  });
+  return axios
+    .get(url)
+    .then((res) => {
+      const { data } = res;
+      if (
+        !data
+          || !data.quoteResponse
+          || !data.quoteResponse.result
+          || data.quoteResponse.result.length === 0
+      ) {
+        return reject(new Error(`Error retrieving info for symbol ${stock}`));
+      }
+      return resolve(data.quoteResponse.result[0]);
+    })
+    .catch((err) => reject(err));
+});
 
-const getStocksInfo = stockList => new Promise((resolve, reject) => {
+const getStocksInfo = (stockList) => new Promise((resolve, reject) => {
   if (!stockList) {
     return reject(Error('Stock symbol list required'));
   }
