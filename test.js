a5fa43a16a6f30b43be97ffe6dd62478a1f96fb0
Luiz Chagas <luiz@crema.us>
2023-05-15 11:37:10 -0500

    Update finance api from v7 to v11 (#22)
    * chore(release): 1.6.3
    
    * chore(release): 1.6.4
    
    * chore(release): 2.0.0
    
    * Update Finance API to v11

--- test.js
+++ test.js
@@ -1,21 +1,21 @@
-const assert = require("assert");
-const si = require("./dist");
+import { getSingleStockInfo, getStocksInfo } from "./dist/index.cjs";
+import assert from "node:assert";
 
 describe("StockInfo", () => {
   describe("#getSingleStockInfo", () => {
     it("should return stock info for AAPL", async () => {
-      const info = await si.getSingleStockInfo("AAPL");
+      const info = await getSingleStockInfo("AAPL");
       assert(typeof info === "object");
-      assert(info.regularMarketPrice > 0);
+      assert(info.regularMarketPrice.raw > 0);
     });
   });
 
   describe("#getStocksInfo", () => {
     it("should return stock info for AMZN and F", async () => {
-      const info = await si.getStocksInfo(["AMZN", "F"]);
+      const info = await getStocksInfo(["AMZN", "F"]);
       assert(info.length === 2);
-      assert(info[0].regularMarketPrice > 0);
-      assert(info[1].regularMarketPrice > 0);
+      assert(info[0].regularMarketPrice.raw > 0);
+      assert(info[1].regularMarketPrice.raw > 0);
     });
   });
 });
